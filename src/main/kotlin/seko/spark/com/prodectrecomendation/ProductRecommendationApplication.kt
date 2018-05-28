8ceb1c5c06e63cfa38770b0c1d007f78759bfc1d
Sergey34 <Sergey3440@gmail.com>
2018-05-28 18:15:30 +0400

    add spring

--- src/main/kotlin/seko/spark/com/prodectrecomendation/ProductRecommendationApplication.kt
+++ src/main/kotlin/seko/spark/com/prodectrecomendation/ProductRecommendationApplication.kt
@@ -1,18 +1,25 @@
 package seko.spark.com.prodectrecomendation
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
-@ComponentScan
-@Configuration
-class ProductRecommendationApplication
+class ProductRecommendationApplication {
+    @Autowired
+    lateinit var productRecommendation: ProductRecommendation
+
+    @PostConstruct
+    fun init() {
+//        productRecommendation.calculate()
+    }
+}
 
 fun main(args: Array<String>) {
-    System.setProperty("spring.profiles.active", "Dev")
-//    val context = AnnotationConfigApplicationContext(ProductRecommendationApplication::class.java)
-//    val productRecommendation = context.getBean(ProductRecommendation::class.java)
-    ProductRecommendation().calculate()
+//    runApplication<ProductRecommendationApplication>(*args)
+    SpringApplication.run(ProductRecommendationApplication::class.java, *args)
 }
